613737898fa1c066a0decf40cda8e117c0a5f903
Thomas Wilhelm <quodos@users.noreply.github.com>
2020-04-11 15:11:24 +0200

    update the addon to work with the new design

--- userscript.js
+++ userscript.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         cardmarket.com wants list addons
 // @namespace    http://tampermonkey.net/
-// @version      0.0.3
+// @version      0.0.4
 // @description  Tampermonkey script that adds several additional functionality to cardmarket.com wants lists.
 // @author       Thomas Wilhelm <https://thomaswilhelm.at>
-// @include      https://www.cardmarket.com/en/Magic/MainPage/showWants*
-// @include      https://www.cardmarket.com/de/Magic/MainPage/showWants*
+// @include      https://www.cardmarket.com/en/Magic/Wants/*
+// @include      https://www.cardmarket.com/de/Magic/Wants/*
 // @grant        none
 // ==/UserScript==
 
@@ -15,7 +15,7 @@
     /// ----------------------------------------
     /// Table changes
 
-    const rows = document.querySelectorAll('table.MKMTable.wantsTable tbody tr');
+    const rows = document.querySelectorAll('#WantsListTable table tbody tr');
     let totalPrice = 0.0;
     let totalPriceWithoutBasics = 0.0;
     let cardsString = '';
@@ -25,16 +25,16 @@
         /// Calculate the total price of all the cards in the wants list and write it into the footer
         Array.prototype.forEach.call(rows, function(el, i){
             const cells = el.children;
-            let amount = parseInt(cells[10].textContent);
+            let amount = parseInt(cells[2].textContent.replace('&nbsp;x', ''));
             if (!amount || amount === NaN) {
                 amount = 1;
             }
-            const value = parseFloat(cells[12].querySelector('div').textContent.replace(',', '.')) * amount;
+            const value = parseFloat(cells[12].querySelector('span').textContent.replace(' €', '').replace(',', '.')) * amount;
             totalPrice += value;
 
             // Ingore cards matching this regex
             const basicLands = /^(island|insel|forest|wald|mountain|gebirge|plains|ebene|swamp|sumpf)(\s\(version\s[0-9]\))?$/gi;
-            const cardName = cells[2].querySelector('a').textContent;
+            const cardName = cells[3].querySelector('a').textContent;
             if (basicLands.test(cardName) === false) {
                 totalPriceWithoutBasics += value;
             }
@@ -42,7 +42,7 @@
             cardsString += `${amount} ${cardName}\n`;
         });
 
-        const footer = document.querySelector('table.MKMTable.wantsTable tfoot tr td');
+        const footer = document.querySelector('#WantsListTable table tfoot tr td');
         footer.style.textAlign = 'right';
         footer.colSpan = "11";
         if (totalPrice === totalPriceWithoutBasics) {
